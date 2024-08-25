0bf7574e3fc4e806d6d942b33ad0208b7531f561
olga <romanovar-151@ya.ru>
2024-08-25 18:12:23 +0300

    Hw8_exception - правки по замечаниям к дз

--- src/main/java/ru/olga/java/basic/homeworks/homework8_exceptions/Application.java
+++ src/main/java/ru/olga/java/basic/homeworks/homework8_exceptions/Application.java
@@ -1,36 +1,43 @@
 package ru.olga.java.basic.homeworks.homework8_exceptions;
 
 public class Application {
-    public static void main(String[] args){
-        String[][] array_2x2 = {{"1", "3"}, {"4", "5"}};
-        String[][] array_3x3 = {{"1", "3", "2"}, {"4", "5", "6"}};
-        String[][] array_2x2_string = {{"1", "a"}, {"4", "5"}};
+    public static void main(String[] args) {
+        String[][] array4x4 = {{"1", "3", "1", "3"},
+                {"4", "5", "1", "3"},
+                {"4", "5", "1", "3"},
+                {"4", "5", "1", "3"}};
+        String[][] array3x3 = {{"1", "3", "2"}, {"4", "5", "6"}};
+        String[][] array4x4string = {{"1", "a", "1", "3"},
+                {"4", "5", "1", "3"},
+                {"4", "5", "b", "3"},
+                {"4", "5", "1", "3"}};
         try {
-            sumStringArray(array_2x2);
-            //sumStringArray(array_3x3);
-            sumStringArray(array_2x2_string);
+            sumStringArray(array4x4);
+            sumStringArray(array3x3);
+            sumStringArray(array4x4string);
         } catch (AppArraySizeException | AppArrayDataException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void sumStringArray(String[][] array) throws AppArraySizeException, AppArrayDataException {
-        if (array.length != 2 || array[0].length != 2 || array[1].length != 2) {
-            throw new AppArraySizeException("Неверный размер массива");
+    public static int sumStringArray(String[][] array) throws AppArraySizeException, AppArrayDataException {
+        for (int i = 0; i < array.length; i++) {
+            if (array.length != 4 || array[i].length != 4) {
+                throw new AppArraySizeException("Неверный размер массива");
+            }
         }
 
-        int[][] int_array = new int[2][2];
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 try {
-                    int_array[i][j] = Integer.parseInt(array[i][j]);
-                    sum += int_array[i][j];
-                } catch (Exception e) {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
                     throw new AppArrayDataException("Неверный формат в ячейке [" + i + "][" + j + "] ");
                 }
             }
         }
         System.out.println("Сумма элементов массива = " + sum);
+        return sum;
     }
 }
