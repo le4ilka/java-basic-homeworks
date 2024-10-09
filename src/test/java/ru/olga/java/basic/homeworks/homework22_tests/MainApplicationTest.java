b54450e904af277b37b39c17013753f8dcc79fb5
olga <romanovar-151@ya.ru>
2024-10-09 12:28:29 +0300

    Hw22_tests - простые тесты, без параметрических тестов

--- src/test/java/ru/olga/java/basic/homeworks/homework22_tests/MainApplicationTest.java
+++ src/test/java/ru/olga/java/basic/homeworks/homework22_tests/MainApplicationTest.java
@@ -1,20 +1,37 @@
 package ru.olga.java.basic.homeworks.homework22_tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainApplicationTest {
 
     @Test
-    void main() {
+    void testPartArray() {
+        int[] expected = {3,2};
+        int[] original = {4,2,1,3,1,3,2};
+        Assertions.assertArrayEquals(expected, MainApplication.partArray(original));
     }
 
     @Test
-    void partArray() {
+    void testPartArrayException() {
+        int[] original = {4,2,4,2,2,3,2};
+        Assertions.assertThrows(RuntimeException.class, ()-> MainApplication.partArray(original));
     }
 
     @Test
-    void checkElements() {
+    void testCheckElementsFalse() {
+        int[] originalFalse = {4,2,1,3,1,3,2};
+        Assertions.assertFalse(MainApplication.checkElements(originalFalse));
+    }
+
+    @Test
+    void testCheckElementsTrue() {
+        int[] originalTrue = {2,2,1,2,1,1,2};
+        Assertions.assertFalse(MainApplication.checkElements(originalTrue));
     }
 }
